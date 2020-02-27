5b93fbc14e03fcaeba2f9ef8eb8164b08f73d8ce
Kuehar <green95251@gmail.com>
2020-02-27 11:35:43 +0900

    added Hashmap solution to twosum.py.

--- TwoSum.py
+++ TwoSum.py
@@ -11,4 +11,34 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if mid == nums[j]:
                     return [i,j]
-        
+
+
+
+
+    """
+    Bruteforce solution
+    Runtime:3436ms
+    Memory:13.6MB
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            mid = target - nums[i]
+            for j in range(i+1,len(nums)):
+                if mid == nums[j]:
+                    return [i,j]
+
+    """
+    Hashmap solution
+    Runtime:48ms
+    Memory:14.1MB
+    """
+    def hashmap(self, nums: List[int], target: int) -> List[int]:
+        hashmap = dict()
+
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if num in hashmap:
+                return [hashmap[num],i]
+            else:
+                hashmap[complement] = i
