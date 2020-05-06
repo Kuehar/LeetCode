9b150304f16c1325cd87673cbf99dd86ea7fb0fd
Kuehar <green95251@gmail.com>
2020-05-06 10:52:55 +0900

    added Majority Element.py

---
+++ Majority Element.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans =  collections.Counter(nums)
+        return ans.most_common(1)[0][0]
+# Runtime: 172 ms, faster than 79.63% of Python3 online submissions for Majority Element.
+# Memory Usage: 15.1 MB, less than 7.14% of Python3 online submissions for Majority Element.
