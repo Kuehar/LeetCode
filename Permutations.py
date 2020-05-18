2ef10f7a11cb2e6683f39c80c6e5e6a7c5b428d7
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-05-18 22:15:38 +0900

    added Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
+# Runtime: 36 ms, faster than 87.99% of Python3 online submissions for Permutations.
+# Memory Usage: 14.1 MB, less than 5.36% of Python3 online submissions for Permutations.
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.dfs(nums, [], ans)
+        return ans
+    
+    def dfs(self, nums, emp, ans):
+        if not nums:
+            ans.append(emp)
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], emp+[nums[i]], ans)
+# Runtime: 32 ms, faster than 96.70% of Python3 online submissions for Permutations.
+# Memory Usage: 13.9 MB, less than 5.36% of Python3 online submissions for Permutations.
