483a7821712049573bec65ba73eabdd66844d9ae
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-06-07 10:08:55 +0900

    Create  Maximum 69 Number.py

---
+++ Maximum 69 Number.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        return int(str(num).replace('6','9',1))
+# Runtime: 28 ms, faster than 73.29% of Python3 online submissions for Maximum 69 Number.
+# Memory Usage: 13.8 MB, less than 58.88% of Python3 online submissions for Maximum 69 Number.
