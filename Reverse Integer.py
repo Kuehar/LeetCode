fc4e8903d11b452576e7e272340d71a96716deda
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-06-19 23:14:51 +0900

    Create Reverse Integer.py

---
+++ Reverse Integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        max_32 = 2**31 - 1
+        if abs(x) > max_32:
+            return 0
+        if x < 0:
+            reverse_int = -int(str(abs(x))[::-1])
+        else:
+            reverse_int = int(str(x)[::-1])
+        if abs(reverse_int) > max_32:
+            return 0
+        else:
+            return reverse_int
+# Runtime: 28 ms, faster than 86.13% of Python3 online submissions for Reverse Integer.
+# Memory Usage: 14 MB, less than 9.77% of Python3 online submissions for Reverse Integer.
