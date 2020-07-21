38ac6b29ca6cc1764f62a0178867a76c416a2789
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-07-22 06:03:52 +0900

    Update Unique Email Addresses.py

--- Unique Email Addresses.py
+++ Unique Email Addresses.py
@@ -8,4 +8,3 @@ class Solution:
         return len(fixed_email)
 # Runtime: 52 ms, faster than 77.31% of Python3 online submissions for Unique Email Addresses.
 # Memory Usage: 14.2 MB, less than 5.16% of Python3 online submissions for Unique Email Addresses.
-実装の面で苦労する、というよりは問題を噛み砕いて考える方が難しいかもしれませんね。
