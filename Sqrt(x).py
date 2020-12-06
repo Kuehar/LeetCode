8d3cbd8fb7d6e5214129c6f907d50a506f2d06ee
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-12-06 22:48:56 +0900

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,12 @@
+# Using library
+
+import math
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(abs(math.sqrt(x)))
+        
+# Runtime: 32 ms, faster than 78.95% of Python3 online submissions for Sqrt(x).
+# Memory Usage: 14.1 MB, less than 78.73% of Python3 online submissions for Sqrt(x).
+
+# I will write scratch Program
