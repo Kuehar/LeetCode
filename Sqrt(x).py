bf82207b8b2e885cac413d69851e9ea54526e521
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-12-06 23:12:58 +0900

    Update Sqrt(x).py

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -9,4 +9,19 @@ class Solution:
 # Runtime: 32 ms, faster than 78.95% of Python3 online submissions for Sqrt(x).
 # Memory Usage: 14.1 MB, less than 78.73% of Python3 online submissions for Sqrt(x).
 
-# I will write scratch Program
+# scratch Program with binary search
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low,high = 0,x
+        while low <= high:
+            mid = low + (high - low) //2
+            if mid*mid <= x < (mid+1) * (mid+1):
+                return mid
+            elif x < mid*mid:
+                high = mid-1
+            else:
+                low = mid+1
+                
+# Runtime: 40 ms, faster than 38.20% of Python3 online submissions for Sqrt(x).
+# Memory Usage: 14.3 MB, less than 13.70% of Python3 online submissions for Sqrt(x).
