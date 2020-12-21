333f73e15f725bb30806969aa6e27070b0748d8f
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-12-22 00:07:04 +0900

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -1,3 +1,4 @@
+# BruteForce
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         for i in range(len(nums)):
@@ -6,3 +7,13 @@ class Solution:
         return max(nums)+1
 # Runtime: 2392 ms, faster than 10.65% of Python3 online submissions for Missing Number.
 # Memory Usage: 15.2 MB, less than 60.09% of Python3 online submissions for Missing Number.
+
+# Using HashSet
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        s = set(nums)
+        for i in range(len(nums)+1):
+            if i not in s:
+                return i
+# Runtime: 124 ms, faster than 84.82% of Python3 online submissions for Missing Number.
+# Memory Usage: 16 MB, less than 5.92% of Python3 online submissions for Missing Number.
