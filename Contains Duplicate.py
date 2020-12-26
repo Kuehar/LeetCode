1a123c345cf3a29a4e2675782b6351e231b16ec1
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-12-26 21:19:58 +0900

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -8,3 +8,15 @@ class Solution:
         return False
 # Runtime: 144 ms, faster than 5.87% of Python3 online submissions for Contains Duplicate.
 # Memory Usage: 19.6 MB, less than 83.37% of Python3 online submissions for Contains Duplicate.
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            else:
+                s.add(num)
+        return False
+# Runtime: 116 ms, faster than 70.88% of Python3 online submissions for Contains Duplicate.
+# Memory Usage: 20.3 MB, less than 39.65% of Python3 online submissions for Contains Duplicate.
