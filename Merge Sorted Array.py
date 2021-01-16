c54afbe845571209e7dc64f369221e02e12ec2af
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-01-16 19:12:51 +0900

    Create Merge Sorted Array.py

---
+++ Merge Sorted Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # mとnが0になるまでループ。各処理の最後で該当する値をディクリメント。
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        nums1[:n] = nums2[:n]
+# Runtime: 36 ms, faster than 74.64% of Python3 online submissions for Merge Sorted Array.
+# Memory Usage: 14.3 MB, less than 33.67% of Python3 online submissions for Merge Sorted Array.
