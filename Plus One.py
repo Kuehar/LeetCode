46748fee6db2cfb1b7d37300e48beeae9673cfcc
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-02-09 00:10:01 +0900

    Update Plus One.py

--- Plus One.py
+++ Plus One.py
@@ -11,3 +11,19 @@ class Solution:
             return digits
 # Runtime: 28 ms, faster than 88.73% of Python3 online submissions for Plus One.
 # Memory Usage: 14.2 MB, less than 45.95% of Python3 online submissions for Plus One.
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+        for i in reversed(range(1,len(digits))):
+            if digits[i] != 10:
+                break
+            digits[i] = 0
+            digits[i-1] += 1
+        else:
+            if digits[0] == 10:
+                digits[0] = 1
+                digits.append(0)
+        return digits
+# Runtime: 28 ms, faster than 90.07% of Python3 online submissions for Plus One.
+# Memory Usage: 14.3 MB, less than 50.47% of Python3 online submissions for Plus One.
