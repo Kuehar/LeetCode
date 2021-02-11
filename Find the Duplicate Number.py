48bbabf419f2faa09e07355a20c176c6a5750cdf
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-02-11 11:15:12 +0900

    Update Find the Duplicate Number.py

--- Find the Duplicate Number.py
+++ Find the Duplicate Number.py
@@ -9,3 +9,18 @@ class Solution:
         return low
 # Runtime: 64 ms, faster than 86.19% of Python3 online submissions for Find the Duplicate Number.
 # Memory Usage: 16.4 MB, less than 35.59% of Python3 online submissions for Find the Duplicate Number.
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        if not nums:
+            return       
+        ans,index = 0,1
+        nums = sorted(nums)
+        for i in range(1,len(nums)):
+            if nums[i] == nums[index-1]:
+                ans = nums[i]
+            else:
+                index += 1
+        return ans
+# Runtime: 68 ms, faster than 55.74% of Python3 online submissions for Find the Duplicate Number.
+# Memory Usage: 16.6 MB, less than 78.10% of Python3 online submissions for Find the Duplicate Number.
