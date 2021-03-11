68c51f1fa81a9615dc408a0d28cf789a12757ac5
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-03-11 22:59:56 +0900

    Create N-th Tribonacci Number.py

---
+++ N-th Tribonacci Number.py
@@ -0,0 +1,9 @@
+# Time Limit Exceeded Solution
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
