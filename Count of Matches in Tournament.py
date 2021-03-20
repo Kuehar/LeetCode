25c2199ead17b9e6ae4201f48a2673651eca6a68
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-03-21 01:32:06 +0900

    Update Count of Matches in Tournament.py

--- Count of Matches in Tournament.py
+++ Count of Matches in Tournament.py
@@ -1,5 +1,7 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         return n-1
+# O(1) Solution.
+# Always this answer is n-1. Sum of matches are always equals to loser.
 # Runtime: 28 ms, faster than 82.44% of Python3 online submissions for Count of Matches in Tournament.
 # Memory Usage: 14.3 MB, less than 40.04% of Python3 online submissions for Count of Matches in Tournament.
