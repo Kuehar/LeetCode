3adc2c27f4432f86a69a54b3f05b0e6e44ffcbf3
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-04-30 23:53:40 +0900

    Update Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -5,3 +5,8 @@ class Solution:
         return ans.most_common(1)[0][0]
 # Runtime: 172 ms, faster than 79.63% of Python3 online submissions for Majority Element.
 # Memory Usage: 15.1 MB, less than 7.14% of Python3 online submissions for Majority Element.
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        return max(cnt.keys(),key=cnt.get)
