790bb1f0e9f4fdcf98e8d9b014ad0e10bff70c33
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-09-11 23:19:49 +0900

    Update N-th Tribonacci Number.py

--- N-th Tribonacci Number.py
+++ N-th Tribonacci Number.py
@@ -17,3 +17,18 @@ class Solution:
         return memo[n]
 # Runtime: 28 ms, faster than 80.87% of Python3 online submissions for N-th Tribonacci Number.
 # Memory Usage: 14.3 MB, less than 13.79% of Python3 online submissions for N-th Tribonacci Number.
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        memo = [0]*(n+1)
+        memo[0], memo[1], memo[2] = 0,1,1
+        for i in range(3,n+1):
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
+        return memo[n]
+# Runtime: 28 ms, faster than 80.61% of Python3 online submissions for N-th Tribonacci Number.
+# Memory Usage: 14.2 MB, less than 70.95% of Python3 online submissions for N-th Tribonacci Number.
