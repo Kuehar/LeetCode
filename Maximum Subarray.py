c0640ebbda6720273d873d0f5a4361089b5e11b2
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-09-19 15:17:33 +0900

    Update Maximum Subarray.py

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -11,3 +11,13 @@ class Solution:
         return max_num
 # Runtime: 60 ms, faster than 93.50% of Python3 online submissions for Maximum Subarray.
 # Memory Usage: 14.5 MB, less than 5.69% of Python3 online submissions for Maximum Subarray.
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0]*len(nums)
+        for i,num in enumerate(nums):            
+            dp[i] = max(dp[i-1] + num, num)
+        return max(dp)
+# Runtime: 68 ms, faster than 58.99% of Python3 online submissions for Maximum Subarray.
+# Memory Usage: 15.1 MB, less than 19.90% of Python3 online submissions for Maximum Subarray.
