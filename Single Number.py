6366fe9c3ba3d08ccb125d94f21e386be181c4ef
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-11-27 17:55:18 +0900

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -6,3 +6,13 @@ class Solution:
         return ans
 # Runtime: 80 ms, faster than 93.89% of Python3 online submissions for Single Number.
 # Memory Usage: 16.5 MB, less than 6.56% of Python3 online submissions for Single Number.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        while len(nums) > 0:
+            num = nums.pop(0)
+            if num in nums:
+                nums.pop(0)
+            else:
+                return num
