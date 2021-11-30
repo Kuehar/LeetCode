9b08d2b5a9e2a5debc68aa3b42b7a874dbe61cee
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-11-30 22:33:56 +0900

    Update Number of Islands.py

--- Number of Islands.py
+++ Number of Islands.py
@@ -1,21 +1,19 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        count = 0
+    def numIslands(self, grid: List[List[str]]) -> int:         
+        # DFS
+        island_sum = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
-                    count += 1
-        return count
-
-    def dfs(self, grid, i, j):
+                if grid[i][j] == "1":
+                    self.dfs(grid,i,j)
+                    island_sum += 1
+        return island_sum
+    
+    def dfs(self,grid,i,j):
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
             return
-        grid[i][j] = '-'
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
+        grid[i][j] = "/"
+        self.dfs(grid,i+1,j)
+        self.dfs(grid,i-1,j)
+        self.dfs(grid,i,j+1)
+        self.dfs(grid,i,j-1)
