fff0507e2aafe54762a32413ca18072c6fed0103
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-03 00:10:42 +0900

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,5 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        # Constraints -2^31 <= x <= 2^31 - 1から
+        # 32bit基準で考える
         max_32 = 2**31 - 1
         if abs(x) > max_32:
             return 0
@@ -7,6 +9,7 @@ class Solution:
             reverse_int = -int(str(abs(x))[::-1])
         else:
             reverse_int = int(str(x)[::-1])
+        
         if abs(reverse_int) > max_32:
             return 0
         else:
