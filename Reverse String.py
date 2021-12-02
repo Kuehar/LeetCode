8b06ce0c5e62301e1a68eb8eb8874c58637d1fdc
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-02 23:49:05 +0900

    Update Reverse String.py

--- Reverse String.py
+++ Reverse String.py
@@ -19,3 +19,18 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         return ''.join(reversed(list(s)))
+    
+   
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        # space complexity O(n)
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def helper(left,right):
+            if left < right:
+                s[left],s[right] = s[right],s[left]
+                helper(left+1,right-1)
+                
+                
+        return helper(0,len(s)-1)
