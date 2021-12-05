e23cb8a14583a747912353483ccd0723b55f4f36
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-06 00:35:21 +0900

    Update House Robber.py

--- House Robber.py
+++ House Robber.py
@@ -7,23 +7,23 @@ class Solution:
 # Runtime: 20 ms, faster than 98.32% of Python3 online submissions for House Robber.
 # Memory Usage: 14.1 MB, less than 9.09% of Python3 online submissions for House Robber.
 
-class Solution:
+cclass Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
+        l = len(nums)
+        if l < 0:
             return 0
-        elif len(nums) == 1:
+        elif l == 1:
             return nums[0]
-        elif len(nums) == 2:
+        elif l == 2:
             return max(nums[0],nums[1])
         
-        dp = [-inf]*len(nums)
-        dp[0],dp[1] = nums[0],max(nums[0],nums[1])
+        dp = [-inf]*l
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
         
-        for i in range(2,len(nums)):
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+        for i in range(2,l):
+            dp[i] = max(dp[i-2]+nums[i],dp[i-1])
             
         return max(dp)
-    
-    
-# Runtime: 28 ms, faster than 87.54% of Python3 online submissions for House Robber.
-# Memory Usage: 14.2 MB, less than 51.36% of Python3 online submissions for House Robber.
+# Runtime: 20 ms, faster than 99.30% of Python3 online submissions for House Robber.
+# Memory Usage: 14.2 MB, less than 52.69% of Python3 online submissions for House Robber.
