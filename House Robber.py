95403e3691dc1c615a6eadd835d1dab5b529fdaf
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-06 00:42:24 +0900

    Update House Robber.py

--- House Robber.py
+++ House Robber.py
@@ -27,3 +27,27 @@ cclass Solution:
         return max(dp)
 # Runtime: 20 ms, faster than 99.30% of Python3 online submissions for House Robber.
 # Memory Usage: 14.2 MB, less than 52.69% of Python3 online submissions for House Robber.
+
+# Recursive and memo
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    
+    def rob(self, nums: List[int]) -> int:
+        self.memo = {}
+        return self.robFrom(0,nums)
+    
+    
+    def robFrom(self,i,nums):
+        # no more houses left to examine.
+        if i >= len(nums):
+            return 0
+        if i in self.memo:
+            return self.memo[i]
+            
+        ans = max(self.robFrom(i+1,nums),self.robFrom(i+2,nums)+nums[i])
+            
+        self.memo[i] = ans
+        return ans
+# Runtime: 32 ms, faster than 66.56% of Python3 online submissions for House Robber.
+# Memory Usage: 14.1 MB, less than 78.62% of Python3 online submissions for House Robber.
