cf99df4b20d04e48a0b23547e2bc484222311508
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-09 22:16:42 +0900

    Update Fibonacci Number.py

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -18,3 +18,16 @@ class Solution:
         if n <= 1:
             return n
         return self.fib(n-1) + self.fib(n-2)
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        
+        cache = [0]*(n+1)
+        cache[1] = 1
+        for i in range(2,n+1):
+            cache[i] = cache[i-1] + cache[i-2]
+        return cache[n]
+# Runtime: 28 ms, faster than 85.51% of Python3 online submissions for Fibonacci Number.
+# Memory Usage: 13.9 MB, less than 97.87% of Python3 online submissions for Fibonacci Number.    
