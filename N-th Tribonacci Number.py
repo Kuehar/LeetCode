42992681e48932e2d477c0faeec95ade1b2097ad
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-09 22:53:30 +0900

    Update N-th Tribonacci Number.py

--- N-th Tribonacci Number.py
+++ N-th Tribonacci Number.py
@@ -10,6 +10,18 @@ class Solution:
     
 class Solution:
     def tribonacci(self, n: int) -> int:
+        if n < 3:
+            return 1 if n else 0
+        
+        x,y,z = 0,1,1
+        for _ in range(n-2):
+            x,y,z = y,z,x+y+z
+        
+        return z
+   
+ 
+class Solution:
+    def tribonacci(self, n: int) -> int:
         memo = collections.defaultdict(int)
         memo[0], memo[1], memo[2] = 0,1,1
         for i in range(3,n+1):
