9cddcbbb23223e9e288ddc059cee57e1af8eecc2
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-11 13:16:45 +0900

    Update Min Cost Climbing Stairs.py

--- Min Cost Climbing Stairs.py
+++ Min Cost Climbing Stairs.py
@@ -9,3 +9,15 @@ class Solution:
         return min(dp[-1],dp[-2])
 # Runtime: 56 ms, faster than 78.97% of Python3 online submissions for Min Cost Climbing Stairs.
 # Memory Usage: 14.5 MB, less than 46.19% of Python3 online submissions for Min Cost Climbing Stairs.
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [inf]*len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2,len(cost)):
+            dp[i] = min(dp[i-1]+cost[i],dp[i-2]+cost[i])
+        print(dp)
+        return min(dp[-1],dp[-2])
+# Runtime: 44 ms
+# Memory Usage: 14.3 MB
