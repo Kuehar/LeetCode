c146d223445eefea5ab4dc9d908be05523b90419
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-11 13:28:43 +0900

    Update Min Cost Climbing Stairs.py

--- Min Cost Climbing Stairs.py
+++ Min Cost Climbing Stairs.py
@@ -21,3 +21,40 @@ class Solution:
         return min(dp[-1],dp[-2])
 # Runtime: 44 ms
 # Memory Usage: 14.3 MB
+# Time complexity: O(N).
+# Space complexity: O(N).
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        def minimum_cost(i):
+            # ベースケース
+            if i <= 1:
+                return 0
+            # 計算済みの内容が含まれていたらそちらを返す
+            if i in memo:
+                return memo[i]
+            
+            # 計算されていない部分の場合は計算し、ハッシュに保存する
+            down_one = cost[i-1] + minimum_cost(i-1)
+            down_two = cost[i-2] + minimum_cost(i-2)
+            memo[i] = min(down_one,down_two)
+            return memo[i]
+        
+        memo = {}
+        return minimum_cost(len(cost))
+
+# ↑の回答は以下のように書くこともできる    
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @cache
+        def minimum_cost(i):
+            if i <= 1:
+                return 0
+            
+            down_one = cost[i - 1] + minimum_cost(i - 1)
+            down_two = cost[i - 2] + minimum_cost(i - 2)
+            return min(down_one, down_two)
+
+        return minimum_cost(len(cost))
+# Time complexity: O(N)
+# Space complexity: O(N)
