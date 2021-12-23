52c2e18865d01739a80a23c256d9e37da64eafda
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-23 18:21:19 +0900

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -17,3 +17,20 @@ class Solution:
             self.dfs(nums[:i]+nums[i+1:], emp+[nums[i]], ans)
 # Runtime: 32 ms, faster than 96.70% of Python3 online submissions for Permutations.
 # Memory Usage: 13.9 MB, less than 5.36% of Python3 online submissions for Permutations.
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def backtrack(first=0):
+            if first == n:
+                ans.append(nums[:])
+            for i in range(first,n):
+                nums[first],nums[i] = nums[i],nums[first]
+                backtrack(first+1)
+                nums[first],nums[i] = nums[i],nums[first]
+        n = len(nums)
+        ans = []
+        backtrack()
+        return ans
+# Runtime: 40 ms, faster than 70.15% of Python3 online submissions for Permutations.
+# Memory Usage: 14.2 MB, less than 89.82% of Python3 online submissions for Permutations.
