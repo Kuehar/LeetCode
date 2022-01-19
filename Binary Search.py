62bd755c5da385415ed2f387ee6e6e2f3062946b
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-01-19 22:47:47 +0900

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -12,3 +12,12 @@ class Solution:
         return -1
 # Runtime: 430 ms, faster than 9.24% of Python3 online submissions for Binary Search.
 # Memory Usage: 15.5 MB, less than 91.51% of Python3 online submissions for Binary Search.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+# Runtime: 342 ms, faster than 26.73% of Python3 online submissions for Binary Search. こっちの方が速い・・・
+# Memory Usage: 15.7 MB, less than 30.42% of Python3 online submissions for Binary Search.
