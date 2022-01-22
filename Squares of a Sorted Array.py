be7daacf5b4ea1abc909626f35e34aa2b087d84b
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-01-22 09:37:39 +0900

    Update Squares of a Sorted Array.py

--- Squares of a Sorted Array.py
+++ Squares of a Sorted Array.py
@@ -3,3 +3,21 @@ class Solution:
         return sorted(x*x for x in nums)
 # Runtime: 200 ms, faster than 98.52% of Python3 online submissions for Squares of a Sorted Array.
 # Memory Usage: 16.3 MB, less than 31.98% of Python3 online submissions for Squares of a Sorted Array.
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0]*n
+        left = 0
+        right = n-1
+        for i in range(n-1,-1,-1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            ans[i] = square * square
+        return ans
+# Runtime: 338 ms, faster than 30.27% of Python3 online submissions for Squares of a Sorted Array.
+# Memory Usage: 16.1 MB, less than 75.08% of Python3 online submissions for Squares of a Sorted Array.
