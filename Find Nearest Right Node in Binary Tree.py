e3109a2c3e918da2d53f86461b083bc0de25299e
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-01-23 11:25:53 +0900

    Update Find Nearest Right Node in Binary Tree.py

--- Find Nearest Right Node in Binary Tree.py
+++ Find Nearest Right Node in Binary Tree.py
@@ -28,3 +28,34 @@ class Solution:
                     next_level.append(node.right)
 # Runtime: 410 ms, faster than 40.94% of Python3 online submissions for Find Nearest Right Node in Binary Tree.
 # Memory Usage: 51.6 MB, less than 74.50% of Python3 online submissions for Find Nearest Right Node in Binary Tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> Optional[TreeNode]:
+        # 番兵を使った方法。各レベルの終端に番兵としてnull(PythonのNone)を入れることで階層の管理を行う。
+        # return cur_level.popleft() if cur_level else None
+        if root is None:
+            return []
+        
+        queue = deque([root,None])
+        while queue:
+            cur = queue.popleft()
+            
+            if cur == u:
+                return queue.popleft()
+            
+            if cur:
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            else:
+                if queue:
+                    queue.append(None)
+# Runtime: 342 ms, faster than 56.04% of Python3 online submissions for Find Nearest Right Node in Binary Tree.
+# Memory Usage: 52.3 MB, less than 48.99% of Python3 online submissions for Find Nearest Right Node in Binary Tree.                    
