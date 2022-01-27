5612f0446bb7c32984444c5458d97013f7ec3fb8
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-01-27 23:29:17 +0900

    Update Find Numbers with Even Number of Digits.py

--- Find Numbers with Even Number of Digits.py
+++ Find Numbers with Even Number of Digits.py
@@ -13,3 +13,18 @@ class Solution:
             else:
                 pass
         return digits
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        ans = 0
+        for n in nums:
+            count = 0
+            while n >= 1:
+                n /= 10
+                count += 1
+            if count % 2 == 0:
+                ans += 1
+            count = 0
+        return ans
+# Runtime: 93 ms, faster than 16.36% of Python3 online submissions for Find Numbers with Even Number of Digits.
+# Memory Usage: 13.9 MB, less than 99.83% of Python3 online submissions for Find Numbers with Even Number of Digits.
