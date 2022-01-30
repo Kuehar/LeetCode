818ea558010ff015cbb7c70ff6092958f363b361
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-01-30 12:37:40 +0900

    Update Merge Sorted Array.py

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -25,3 +25,21 @@ class Solution:
         nums1.sort()
 # Runtime: 146 ms, faster than 23.78% of Python3 online submissions for Duplicate Zeros.
 # Memory Usage: 14.8 MB, less than 61.64% of Python3 online submissions for Duplicate Zeros.
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        copied_nums1 = nums1[:m]
+        
+        p1 = p2 = 0
+        for p in range(n+m):
+            if p2 >= n or (p1 < m and copied_nums1[p1] <= nums2[p2]):
+                nums1[p] = copied_nums1[p1]
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
+# Runtime: 70 ms, faster than 10.18% of Python3 online submissions for Merge Sorted Array.
+# Memory Usage: 13.9 MB, less than 99.78% of Python3 online submissions for Merge Sorted Array.
